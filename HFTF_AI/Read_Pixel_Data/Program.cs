912ce0354cc5034569a315458e3775902d520f65
Patrick McCaffrey <allknowingq@gmail.com>
2018-04-22 01:17:05 -0400

    HFTF_AI Solution Created
    Created HFTF_AI solution and one project folder. Created solution on MacOS version of Visual Studio

---
+++ HFTF_AI/Read_Pixel_Data/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Read_Pixel_Data
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
