0e9d1867c36a87b76e4068a7ec50ae7d6d17d43e
Patrick McCaffrey <allknowingq@gmail.com>
2018-05-15 17:36:52 -0400

    Windows Test Solution Not Working

---
+++ Test-HFTF-AI/Test-HFTF-AI/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_HFTF_AI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        private static Bitmap CaptureImage(int x, int y)
+        {
+            Bitmap b = new Bitmap(100, 100);
+            using (Graphics g = Graphics.FromImage(b))
+            {
+                g.CopyFromScreen(x, y, 0, 0, new Size(100, 100), CopyPixelOperator.SourceCopy);
+            }
+            return b;
+        }
+    }
+}
