bec5f316dff85ceea90732413ec380e9fb203299
Patrick McCaffrey <allknowingq@gmail.com>
2018-05-15 22:57:27 -0400

    Working ScreenCap Test

--- Test-HFTF-AI/Test-HFTF-AI/Program.cs
+++ Test-HFTF-AI/Test-HFTF-AI/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
+using System.Diagnostics;
+using System.Drawing.Imaging;
 
 namespace Test_HFTF_AI
 {
@@ -11,8 +13,13 @@ namespace Test_HFTF_AI
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Starting screen capture test...");
+            TestCapture();
+            Console.WriteLine("Saving test image...");
+            ExportCapture();
         }
+
+        public static readonly Stopwatch sw = new Stopwatch();
 
         private static Bitmap CaptureImage(int x, int y)
         {
@@ -22,6 +29,24 @@ namespace Test_HFTF_AI
                 g.CopyFromScreen(x, y, 0, 0, new Size(100, 100), CopyPixelOperation.SourceCopy);
             }
             return b;
+        }
+
+        private static void TestCapture()
+        {
+            Bitmap bmp = null;
+            sw.Restart();
+            for (int i = 0; i < 100; i++)
+            {
+                bmp = CaptureImage(0, 0);
+            }
+            sw.Stop();
+            Console.WriteLine(sw.ElapsedMilliseconds);
+        }
+
+        private static void ExportCapture()
+        {
+            Bitmap bmp = CaptureImage(0, 0);
+            bmp.Save("TestImage.png", ImageFormat.Png);
         }
     }
 }
